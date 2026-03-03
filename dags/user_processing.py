1bf34ed738513b96d86314ba2f8c1fc6fad17785
Karan Solanki <solankikaran05@gmail.com>
2026-03-03 11:14:04 +0530

    initial commit

---
+++ dags/user_processing.py
@@ -0,0 +1,60 @@
+from airflow.sdk import dag, task
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sdk.bases.sensor import PokeReturnValue
+from airflow.providers.standard.operators.python import PythonOperator
+import requests
+
+def _extract_user(ti):
+    fake_user = ti.xcom_pull(task_ids = "is_api_available")
+    return {
+        "id": fake_user["id"],
+        "first_name": fake_user["personalInfo"]["firstName"],
+        "last_name": fake_user["personalInfo"]["lastName"],
+        "email": fake_user["personalInfo"]["email"],
+    }
+
+@dag
+def user_processing():
+
+    # task 1 - create table in a PostgreSQLDB
+    create_table = SQLExecuteQueryOperator(
+        task_id = "create_table",
+        conn_id = "postgres",
+        sql="""
+            CREATE TABLE IF NOT EXISTS users (
+                id INT PRIMARY KEY,
+                first_name VARCHAR(255),
+                last_name VARCHAR(255),
+                email VARCHAR (255),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        """        
+    )
+
+    # task 2 - verifies if API is available using Sensor and gets the response 
+    @task.sensor(poke_interval=30, timeout=300)
+    def is_api_available() -> PokeReturnValue:
+        response = requests.get("https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json")
+        print(response.status_code)
+        if response.status_code == 200:
+            condition = True
+            fake_user = response.json()
+        else:
+            condition = False
+            fake_user = None
+
+        return PokeReturnValue(is_done=condition, xcom_value=fake_user)
+    
+    is_api_available()
+
+    # task 3 - extract user from API
+    extract_user = PythonOperator(
+        task_id = "extract_user",
+        python_callable = _extract_user
+    )
+
+
+
+
+
+user_processing()
